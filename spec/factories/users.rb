7803c563723ff3960541f2d35a4f73fcd362b666
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-02 16:03:36 +0900

    テスト_準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password              {password}
+    password_confirmation {password}
+    first_name_zenkaku    {'加藤'}
+    first_name_kana       {'カトウ'}
+    last_name_zenkaku     {'泰平'}
+    last_name_kana        {'タイヘイ'}
+    birthday              {'1987/09/15'}
+  end
+end
