eca5ea425721550f61d9b97e13de74ff33123587
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-03 06:58:53 +0900

    itemsテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :product_category_id, presence: true
+  validates :product_condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :delivery_from_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true
+  
+  belongs_to :user
+  #has_one :order
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ApplicationRecord
          validates :birthday, presence: true
          
          has_many :items
-         has_many :orders
+         #has_many :orders
 
 end
