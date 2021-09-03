62a9f1707b25f0c9b0a41e8f746ee97de270b93c
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-03 11:48:05 +0900

    ActiveHash作成 v2（商品状態、配送料負担、発送元地域、発送日数）

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,20 @@ class Item < ApplicationRecord
 
   validates :product_name, presence: true
   validates :product_description, presence: true
-  validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :product_condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :delivery_from_id, presence: true
-  validates :delivery_day_id, presence: true
+  validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :product_condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_from_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true
   
   has_one_attached :image
   belongs_to :user
+  
   belongs_to :product_category
+  belongs_to :product_condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_from
+  belongs_to :delivery_day
   #has_one :order
 end
