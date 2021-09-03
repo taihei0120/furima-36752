9f779ce5dcda1a738d1aff5df8d2308ca09976b6
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-03 11:00:13 +0900

    ActiveHash作成 v1（商品カテゴリー）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   validates :product_name, presence: true
   validates :product_description, presence: true
-  validates :product_category_id, presence: true
+  validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :product_condition_id, presence: true
   validates :delivery_fee_id, presence: true
   validates :delivery_from_id, presence: true
@@ -10,5 +12,6 @@ class Item < ApplicationRecord
   
   has_one_attached :image
   belongs_to :user
+  belongs_to :product_category
   #has_one :order
 end
