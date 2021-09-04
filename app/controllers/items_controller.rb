0f0a9b2481f1b02608df1679553986ecd393c11d
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-04 21:24:33 +0900

    アクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @item = Item.order("created_at DESC")
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
     else
       render 
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
