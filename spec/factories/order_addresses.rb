2fbe0c1fc8dfbd6a6f8547001dd47139acfc1123
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-05 15:19:06 +0900

    FactoryBot作成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_code         { '123-4567' }
+    delivery_from_id    { 2 }
+    town                { '港区' }
+    home_number         { '1-1' }
+    building            { '東京ハイツ' }
+    telephone           { '03-1234-5678' }
+    user_id             { 2 }
+    item_id             { 2 }
   end
+
 end
