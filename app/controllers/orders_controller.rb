09470e43287621eb628bccb89912a61ae84c681d
taihei0120 <89062071+taihei0120@users.noreply.github.com>
2021-09-05 15:10:27 +0900

    バリデーションとコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
   
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :delivery_from_id, :town, :home_number, :building, :telephone, :order_id).merge(user_id: current_user.id, )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor
+  attr_accessor :postal_code, :delivery_from_id, :town, :home_number, :building, :telephone, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :town
+    validates :home_number
+    validates :building
+    validates :telephone
+    validates :user_id
+    validates :item_id
+  end
+    validates :delivery_from_id, presence: true, numericality: { other_than: 1 }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, delivery_from_id: delivery_from_id, town: town, home_number: home_number, building: building, telephone: telephone, order_id: order_id)
+  end
+
 end
